4fe8bbde4179aa9af16ec78d05baeda09660340e
issac-kondreddy <ixk93660@ucmo.edu>
2023-12-16 10:49:48 -0600

    Course Done

--- File input output/Read all lines/read_all_lines.py
+++ File input output/Read all lines/read_all_lines.py
@@ -1,5 +1,8 @@
 with open("input.txt", "r") as infile:
     # TODO: read all lines from input.txt into the list called lines_list
+    lines_list = []
+    for lines in infile:
+        lines_list.append(lines)
 
 if __name__ == "__main__":
     print(lines_list)
